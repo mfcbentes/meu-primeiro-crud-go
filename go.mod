79f39472083047a919454f455632a3cfcc8515ed
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2024-04-13 20:41:09 -0300

    :sparkles: feat: Padronizando erros

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mfcbentes/meu-primeiro-crud-go
+
+go 1.22.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	fmt.Println(os.Getenv("TEST"))
+}
