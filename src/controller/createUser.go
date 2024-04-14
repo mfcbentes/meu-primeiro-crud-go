ddbf6e9cb5cc658d43c27ad2725528d8c787fc21
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2024-04-13 23:15:27 -0300

    :sparkles: feat: Criando objetos do usuário

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mfcbentes/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/mfcbentes/meu-primeiro-crud-go/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
 
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma incorreta")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrects filds, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+
 }
