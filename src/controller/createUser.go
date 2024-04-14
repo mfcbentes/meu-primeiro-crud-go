299381d91d7ab7ab0de6464705bdfb45891e0429
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2024-04-13 22:15:59 -0300

    :test_tube: test: Testando as rotas

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,12 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mfcbentes/meu-primeiro-crud-go/src/configuration/rest_err"
+)
 
 func CreateUser(c *gin.Context) {
-	// some code
+
+	err := rest_err.NewBadRequestError("Você chamou a rota de forma incorreta")
+	c.JSON(err.Code, err)
 }
