1fbf6d64c15fb0aeac35cfe4d84fdf0fe79706b0
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2024-04-14 00:13:02 -0300

    :sparkles: feat: Validando campos da requisição

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -14,7 +14,7 @@ func CreateUser(c *gin.Context) {
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrects filds, error=%s\n", err.Error()))
+			fmt.Sprintf("There are some incorrects filds. Error = %s", err.Error()))
 		c.JSON(restErr.Code, restErr)
 		return
 	}
